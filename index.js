db5fc4e7577d202a03bd55d8968e009de733ec09
dubem-ux <dubemrazzle@gmail.com>
2023-08-01 13:54:17 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// create your  forms, select elements buttons and the divs
+// grab all elements in the js file
+// when you click register button, 
+// grab the contents of the form using a constructor
+// store each value in the local storage
+// fetch the contents of the local storage as page loads
+// filter the data and get all unique models and prizes and push 
+// them into different arrays
+// create options for each value of respective models and prizes nand 
+// append them to the corresponding select elements
+// when the value of select elements changes, filter the recieved elements 
+// array and display all data with matching values
+// display corresponding values in the div provided
+// forther filter the values from the div data by colors and screensize
+// display the final data in the div provided as the 
+// recommended laptop to be bought
+
